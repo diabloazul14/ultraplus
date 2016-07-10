4bc073b9db196c0f7514fcf2c008b1ead67fdabd
diabloazul14 <han11077@byui.edu>
2016-07-09 19:56:41 -0600

    Saving progress only, stuck at what to do with samples

--- main.py
+++ main.py
@@ -3,13 +3,44 @@ import wav
 import midi
 import signalAnalysis as sa
 import sys
+import wave
 
 #Wav file data grabbing
-wavFile = wav.wav(str(sys.argv[1]))
-samples = wavFile.parseSamples()
+# wavFile = wav.wav(str(sys.argv[1]))
+# samples = wavFile.parseSamples()
+wavFile = wave.open(str(sys.argv[1]), 'rb')
+rawsamples = wavFile.readframes(wavFile.getnframes())
+hexsamples = []
+
+i = 0
+while i < len(rawsamples):
+	newsample = rawsamples[i] + rawsamples[i + 1]
+	hexsamples.append(newsample.encode('hex'))
+	i += 2
+
+presamples = []
+for sample in hexsamples:
+	presamples.append(wav.littleEndianStringToInteger(sample))
+
+
+samples = []
+if wavFile.getnchannels() == 1:
+	samples = presamples
+if wavFile.getnchannels() == 2:
+	i = 0
+	lenPreSamples = len(presamples)
+	while i < lenPreSamples:
+		samples.append(wav.average(presamples[i], presamples[i + 1]))
+		i += 2
+
+crossingPoints = 0
+for sample in samples:
+	if sample < 30:
+		crossingPoints += 1
+print("crossingPoints: " + str(crossingPoints))
 
 #Analyze the Samples
-#analyzer = sa.signalAnalysis(dataBlocks)
+#analyzer = sa.signalAnalysis(samples)
 
 #Convert the samples into midi file
 
